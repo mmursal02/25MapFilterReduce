5dbf34e9b14506bb7927c0314811704098fe8f7e
mmursal02 <92879732+mmursal02@users.noreply.github.com>
2022-05-16 20:57:58 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
+
+import emojipedia from "./emojipedia";
+
+const returnedMessage = emojipedia.map(function (text) {
+  console.log(text.meaning);
+  return text.meaning.substring(1, 4);
+});
+console.log(returnedMessage);
